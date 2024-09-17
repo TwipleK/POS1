44bddb1f9c7a5c2a83d7d8203703460711ca5ae7
Clemens Auinger <clemensauinger@gmail.com>
2024-09-17 11:37:25 +0200

    Methods and Variables created

--- EratosthenesPrimeSieve/src/net/htlgkr/pos2/eratosthenesPrimeSieve/EratosthenesPrimeSieve.java
+++ EratosthenesPrimeSieve/src/net/htlgkr/pos2/eratosthenesPrimeSieve/EratosthenesPrimeSieve.java
@@ -2,9 +2,20 @@ package net.htlgkr.pos2.eratosthenesPrimeSieve;
 
 public class EratosthenesPrimeSieve implements PrimeSieve{
 
+    private int N;
+    boolean[] primes;
+
+
     @Override
     public boolean isPrime(int p) {
         return false;
+    }
+
+    public EratosthenesPrimeSieve(int N) {
+        this.N = N;
+        for (int i = 2; i < N+1; i++) {
+            primes[i] = true;
+        }
     }
 
     @Override
